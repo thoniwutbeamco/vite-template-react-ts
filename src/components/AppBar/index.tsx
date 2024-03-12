1b073872887f688009124d8052cf1c7064413a71
Tony <99641670+thoniwutbeamco@users.noreply.github.com>
2024-03-12 20:50:13 +0700

    create template tsx class and css

---
+++ src/components/AppBar/index.tsx
@@ -0,0 +1,7 @@
+import "./index.css";
+
+function AppBar() {
+  return <div className="common_theme">AppBar</div>;
+}
+
+export default AppBar;
---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,7 @@
+import "./index.css";
+
+function Footer() {
+  return <div className="footer">Footer</div>;
+}
+
+export default Footer;
